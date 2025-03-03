7456c3a2f65d5e5c84ab031050d463338d1ad48f
Emmanuel <emmanuelsilano@gmail.com>
2025-03-03 16:48:39 -0500

    start

---
+++ jobbies_jobbies/src/main/java/com/jobbies/jobbies/JobbiesApplication.java
@@ -0,0 +1,13 @@
+package com.jobbies.jobbies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobbiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobbiesApplication.class, args);
+	}
+
+}
---
+++ jobbies_jobbies/src/test/java/com/jobbies/jobbies/JobbiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobbies.jobbies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobbiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
